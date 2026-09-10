25beb249769c014e4ecfb3a82ed5f6de96b86da5
DJAYI <danilo.arenasyi@gmail.com}>
2026-09-10 05:17:16 +0000

    feat: Add AssetCardTemplateSection model and migration for asset card template sections

--- database/migrations/tenant/2026_09_10_051353_create_asset_card_template_sections_table.php
+++ database/migrations/tenant/2026_09_10_051353_create_asset_card_template_sections_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('assets', function (Blueprint $table) {
+        Schema::create('asset_card_template_sections', function (Blueprint $table) {
             $table->id();
-            $table->string('code')->unique();
-            $table->boolean('is_active')->default(true);
-            
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('assets');
+        Schema::dropIfExists('asset_card_template_sections');
     }
 };
